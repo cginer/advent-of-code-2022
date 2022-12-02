98234d6df1493fdd7a60a788b980294541f09261
Carla Giner <carla.giner@genomicsplc.com>
2022-12-02 22:21:27 +0000

    Separate days... it is going to get long

--- day_2.R
+++ day_2.R
@@ -2,48 +2,6 @@ library(dplyr)
 library(tidyr)
 library(readr)
 
-################################################################################
-# Advent of code - Day 1
-################################################################################
-
-elf_calories <- function(filename){
-
-  data <- read.table(filename, blank.lines.skip = FALSE) %>%
-    rename(calory_list = V1)
-
-  n_elves <- data %>% filter(is.na(calory_list)) %>% nrow() + 1
-
-  tidy_data <- data %>%
-    summarise(list = paste(calory_list, collapse = ",")) %>%
-    separate(list, sep = ",NA,", into = paste("elf", 1:n_elves, sep = "_")) %>%
-    pivot_longer(starts_with("elf"), names_to = "elf", values_to = "calories") %>%
-    separate_rows(calories, sep = ",", convert = TRUE) %>%
-    group_by(elf) %>%
-    summarise(total_calories = sum(calories))
-
-  the_most_calories <- tidy_data %>%
-    arrange(total_calories) %>%
-    tail(1) %>%
-    pull(total_calories)
-
-  total_top_three <- tidy_data %>%
-    arrange(total_calories) %>%
-    tail(3) %>%
-    pull(total_calories) %>%
-    sum()
-
-  return(list(most = the_most_calories, top_three = total_top_three))
-
-}
-
-
-elf_calories("day_one_input.txt")
-
-
-################################################################################
-# Advent of code - Day 2
-################################################################################
-
 total_score <- function(guide_filename, opponent_dictionary, your_dictionary,
                         shape_combinations){
 
