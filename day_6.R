df5342d274780b4b95e8a3881b4db7d7735015eb
Carla Giner <carla.giner@genomicsplc.com>
2022-12-06 10:57:11 +0000

    Day 6 part 2

--- day_6.R
+++ day_6.R
@@ -1,18 +1,23 @@
 library(dplyr)
 
 
-datastream <- readLines("day_6_input.txt")
+find_marker <- function(filename, marker_length){
+  datastream <- readLines("day_6_input.txt")
 
-signal <- strsplit(datastream, split = "")[[1]]
+  signal <- strsplit(datastream, split = "")[[1]]
 
-for (i in 4:(length(signal))) {
-  possible_marker <- signal[(i-3):i]
+  for (i in marker_length:(length(signal))) {
+    possible_marker <- signal[(i-marker_length+1):i]
 
-  n_unique_letters <- possible_marker %>% unique() %>% length()
+    n_unique_letters <- possible_marker %>% unique() %>% length()
 
-  if(n_unique_letters %in% 4){
-    print(paste0("We've got a marker in position ", i, ":"))
-    print(possible_marker)
-    break
+    if(n_unique_letters %in% marker_length){
+      print(paste0("We've got a marker in position ", i, ":"))
+      print(possible_marker)
+      break
+    }
   }
 }
+
+find_marker("day_6_input", 4)
+find_marker("day_6_input", 14)
