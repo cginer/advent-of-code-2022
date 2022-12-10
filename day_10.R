9feea1452c2555304717d7ac0b7b11db700c1455
Carla Giner <carla.giner@genomicsplc.com>
2022-12-10 22:27:08 +0000

    Day 10 part 2

--- day_10.R
+++ day_10.R
@@ -26,6 +26,30 @@ total_interesting_signal_strengths <- function(cycle_signal_strength){
   return(total_signal)
 }
 
+get_screen <- function(cycle_signal_strength){
+  CRT_sprite_position <- bind_rows(tibble(cycle = 1L, X_value = 1L),
+                               cycle_signal_strength %>%
+                                 select(cycle, X_value)) %>%
+    mutate(CRT_row = ceiling(cycle/40),
+           CRT_pixel = (cycle - 1) %% 40) %>%
+    group_by(cycle) %>%
+    mutate(result = if_else(CRT_pixel %in% c(X_value - 1, X_value, X_value + 1),
+                            "#",
+                            ".")) %>%
+    ungroup()
+
+  message <- CRT_sprite_position %>%
+    select(CRT_row, CRT_pixel, result) %>%
+    group_by(CRT_row) %>%
+    summarise(paste(result, collapse = ""))
+
+  return(message)
+}
+
 cycle_signal_strength <- get_signal_strengths("day_10_input.txt")
 
+# Part 1
 total_interesting_signal_strengths(cycle_signal_strength)
+
+# Part 2
+get_screen(cycle_signal_strength)
